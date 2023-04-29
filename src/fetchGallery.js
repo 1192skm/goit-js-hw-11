538bbb54bde3c7023798d255a0d3ce7a12d284d3
Kostia Shaposhnyk <118116537+1192skm@users.noreply.github.com>
2023-04-29 14:39:46 +0300

    Without try/catch

--- src/fetchGallery.js
+++ src/fetchGallery.js
@@ -3,13 +3,13 @@ export const perPage = 40;
 export async function fetchGallery(search, page = 1) {
   const BASE_URL = 'https://pixabay.com/api';
   const KEY_API = '35783392-ced7b2b0963a7e7ac45fdf9cc';
-    try {
+    
         const resp = await axios.get(
             `${BASE_URL}?key=${KEY_API}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
         );
 
         return await resp.data;
-    } catch (err){console.log(err);}
+     
   // return fetch(
   //   `${BASE_URL}?key=${KEY_API}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
   // ).then(resp => {

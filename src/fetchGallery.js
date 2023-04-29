0ba49ee2e268b5cae8fca59d134acfb9128adee3
Kostia Shaposhnyk <118116537+1192skm@users.noreply.github.com>
2023-04-29 13:49:43 +0300

    Separate fetchGallery function

---
+++ src/fetchGallery.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export async function fetchGallery(search, page = 1) {
+  const BASE_URL = 'https://pixabay.com/api';
+  const KEY_API = '35783392-ced7b2b0963a7e7ac45fdf9cc';
+
+  const resp = await axios.get(
+    `${BASE_URL}?key=${KEY_API}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+  );
+
+  return await resp.data;

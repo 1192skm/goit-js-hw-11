b576a31e353d97ed73a65e247c4a77ddd062cbb3
Kostia Shaposhnyk <118116537+1192skm@users.noreply.github.com>
2023-04-29 14:41:45 +0300

    Refactoring fetchGallery

--- src/fetchGallery.js
+++ src/fetchGallery.js
@@ -3,13 +3,12 @@ export const perPage = 40;
 export async function fetchGallery(search, page = 1) {
   const BASE_URL = 'https://pixabay.com/api';
   const KEY_API = '35783392-ced7b2b0963a7e7ac45fdf9cc';
-    
-        const resp = await axios.get(
-            `${BASE_URL}?key=${KEY_API}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
-        );
 
-        return await resp.data;
-     
+  const resp = await axios.get(
+    `${BASE_URL}?key=${KEY_API}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+  );
+
+  return await resp.data;
   // return fetch(
   //   `${BASE_URL}?key=${KEY_API}&q=${search}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
   // ).then(resp => {
